d5ffc43239fa511cddb27fb4e5f5b82d7d6609cc
TheWitness <thewitness@cacti.net>
2026-09-16 18:34:39 -0400

    Align composer.json/composer.lock and refresh copyright years
    - Replace composer.json with the Cacti-standard project manifest (adds
      ext-gmp/json/ldap/mbstring/openssl/pcntl/posix/sockets requirements,
      pear/net_dns2, dev tooling: php-cs-fixer, phplint, phpstan) and set
      minimum PHP to ^8.2
    - Regenerate composer.lock to match
    - Bump RRDP_PHP_VERSION_REQUIRED to 80200 (PHP 8.2) to match composer.json
    - Update 'Copyright (C) 2004-2024 The Cacti Group' headers to 2004-2026
      across all PHP source files and README.md

--- include/global.php
+++ include/global.php
@@ -1,7 +1,7 @@
 <?php
 /*
  +-------------------------------------------------------------------------+
- | Copyright (C) 2004-2024 The Cacti Group                                 |
+ | Copyright (C) 2004-2026 The Cacti Group                                 |
  |                                                                         |
  | This program is free software; you can redistribute it and/or           |
  | modify it under the terms of the GNU General Public License             |
@@ -27,7 +27,7 @@ define('COPYRIGHT_YEARS_SHORT', '(c) 2004-' . date('Y') . ' - The Cacti Group');
 
 define('RRDP_VERSION', '1.2.17');
 define('RRDP_VERSION_FULL', 'RRDtool Proxy Server v' . RRDP_VERSION . ', ' . COPYRIGHT_YEARS . "\r\n");
-define('RRDP_PHP_VERSION_REQUIRED', 80100);
+define('RRDP_PHP_VERSION_REQUIRED', 80200);
 
 define('RRD_OK', 'OK u:0.00');
 define('RRD_ERROR', 'ERROR:');
